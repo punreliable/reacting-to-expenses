3a59a0c290febf390d1056d0f4b582ebbf193214
Brian <briansmith@rocketcentral.com>
2023-01-23 09:48:25 -0500

    Added vanilla JS to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,10 +2,19 @@ import './ExpenseItem.scss';
 import moment from 'moment';
 
 function ExpenseItem( props ) {
+
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    
+    const year = props.date.getFullYear();
+
     return (
         <div className="expense-item">
             <div className="expense-item__date">
-                {moment(props.date).format("MM/DD/YYYY")}
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
             </div>
             <div className="expense-item__description">
                 <h3>{props.title}</h3>
