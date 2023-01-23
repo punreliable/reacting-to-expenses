2825b944c694c9158d9ae9db1ef0b6fe7258e455
Brian <briansmith@rocketcentral.com>
2023-01-23 11:10:32 -0500

    Split component into multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,10 @@
 import './ExpenseItem.scss';
-import moment from 'moment';
+import ExpenseDate from './ExpenseDate.js';
 
 function ExpenseItem( props ) {
-
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    
-    const year = props.date.getFullYear();
-
     return (
         <div className="expense-item">
-            <div className="expense-item__date">
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h3>{props.title}</h3>
             </div>
