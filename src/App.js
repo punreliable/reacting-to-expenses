b8ed66b695d5bc4520732d3aa80836d9f16c81bb
Brian <briansmith@rocketcentral.com>
2023-01-24 17:16:19 -0500

    Module 4 is complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import NewExpense from './components/NewExpense/NewExpense';
 
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -26,6 +26,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   // return React.createElement(
   //   'div',
   //   {},
@@ -35,8 +40,7 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense />
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,13 +1,22 @@
-import './NewExpense.scss';
 import React from 'react';
+
 import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
 
-function NewExpense() {
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  };
 
-    return <div className="wrapper-form">
-        <ExpenseForm />
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
-
-}
+  );
+};
 
 export default NewExpense;
