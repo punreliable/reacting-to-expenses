ad2e83dc250c55aa2e171c72274fc65e68888f51
Brian <briansmith@rocketcentral.com>
2023-01-26 17:20:00 -0500

    WiP Chart and Char Bar

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,9 +1,20 @@
+
 import './Chart.scss';
+
+import ChartBar from './ChartBar';
+
 
 const Chart = props => {
 
     return <div className="chart">
-
+        {props.dataPoints.map(dataPoint => 
+            <ChartBar 
+                key={dataPoint.label}
+                value={dataPoint.value}
+                maxValue={null} 
+                label={dataPoint.label}
+            />
+        )}
     </div>
 };
 
---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,11 @@
+
+import './ChartBar.scss';
+
+const ChartBar = props => {
+
+    return <div className="chart-bar">
+
+    </div>
+};
+
+export default ChartBar;
